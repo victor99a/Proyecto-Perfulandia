4cf54dd863b8ea7104f29302538f8e1c2dde0599
victor99a <vi.barreraj@duocuc.cl>
2025-05-23 22:14:15 -0400

    commit inicial para que cloneen

--- src/main/java/com/perfulandia/perfulandia_vm/service/UsuarioService.java
+++ src/main/java/com/perfulandia/perfulandia_vm/service/UsuarioService.java
@@ -1,1 +1,29 @@
-// UsuarioService.java - clase vacía para implementar
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private  UsuarioRepository usuarioRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Usuario registrarUsuario(Usuario usuario) {
+        // Encriptar la contraseña
+        usuario.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
+
+        // Asignar rol predeterminado si no se ha asignado
+        if (usuario.getRol() == null) {
+            usuario.setRol(Usuario.Rol.usuario); // Rol predeterminado
+        }
+
+        // Guardar el usuario en la base de datos
+        return usuarioRepository.save(usuario);
+    }
+}
