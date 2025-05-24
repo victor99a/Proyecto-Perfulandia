000b67a85af027e1a628d576b1051eaf1408af12
victor99a <vi.barreraj@duocuc.cl>
2025-05-24 00:55:57 -0400

    Regularización de microservicio de usuario - correccion de errores

---
+++ src/main/java/com/perfulandia/perfulandia_vm/ProyectoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_vm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFullstackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFullstackApplication.class, args);
+
+    }
+}
